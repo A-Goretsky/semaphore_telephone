cff131ca1579638e4009799a4173095ed3f0a718
xd <fortune.soleil@gmail.com>
2016-12-11 11:33:56 -0500

    added a handful of comments

--- client.c
+++ client.c
@@ -19,37 +19,48 @@ union semun {
 };
 */
 int main() {
+  //same key as control; semaphore magic
   int semkey = ftok("makefile", 23);
-  int semid = semget(semkey, 1, 0644);
+  int semid = semget(semkey, 1, 0);
   struct sembuf sb;
   sb.sem_num = 0;
   sb.sem_flg = SEM_UNDO;
+  //wait for the semaphore to become available
   sb.sem_op = 0;
   semop(semid, &sb, 1);
+  //down it as soon as it is
   sb.sem_op = -1;
   semop(semid, &sb, 1);
 
+  //shared memory magic
   int shmkey = ftok("control.c", 12);
-  int shmid = shmget(shmkey, sizeof(long), 0644);
+  int shmid = shmget(shmkey, sizeof(long), 0);
+  //Last Update Length
   long *lul = shmat(shmid, 0, 0);
+  //Last Update
   char *lu = (char *)malloc(*lul + 1);
   
-  int fd = open("story.txt", O_RDWR | O_APPEND, 0644);
+  //locating point in story and displaying last addition
+  int fd = open("story.txt", O_RDWR | O_APPEND);
   lseek(fd, -(*lul), SEEK_END);
   read(fd, lu, *lul);
   printf("Last Update: %s\n", lu);
   printf("Type Here: ");
 
-  char nu[512];
+  //Next Update
+  char nu[256];
   fgets(nu, 256, stdin);
 
+  //since you need it for detach anyway
   *lul = strlen(nu);
 
+  //cursor is at end of file, write length of input
   write(fd, nu, *lul);
   close(fd);
   
   shmdt(lul);
-
+  
+  //send the semaphore back skywards
   sb.sem_op = 1;
   semop(semid, &sb, 1);
   
