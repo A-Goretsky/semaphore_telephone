94cf734a178b807a9cfa6cb67d97a54c37487b91
Anton <anton.goretsky@gmail.com>
2016-12-10 23:30:40 -0500

    most control.c stuff done

--- control.c
+++ control.c
@@ -9,7 +9,6 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <rand.h>
 
 #define DEBUG 0
 /*union semun {
@@ -19,34 +18,86 @@
     struct seminfo *__buf
 };*/
 
+void showstory() {
+    char *storage;
+    int file;
+    file = open("story.txt", O_RDONLY);
+    int num = lseek(file, 0, SEEK_END);
+    read(file, &storage, num);
+    printf("HELLO");
+    printf("%s\n", storage);
+    close(file);
+}
+
 int main(int argnum, char **args) {
+
+    /*if (argnum != 2) {
+        printf("Incorrect arguments. Please type program name followed by one flag. -c, -v, or -r");
+        return 1;
+    }
     //random num gen for key.
     //sranddev();
     //int randnum = rand();
-    int key;
-    int shmid;
+
+    //things needed
+    int f;
+    int memkey;
+    int semkey;
     int semid;
+    int shmid;
+    int semscval;
+    int shmcval;
+    struct shmid_ds shmemctl;
+
+    //creation of keys
+    memkey = ftok("control.c", 12);
+    if (DEBUG) printf("Mem key created. Key: %d\n", memkey);
+    semkey = ftok("makefile", 23);
+    if (DEBUG) printf("Sem key created. Key: %d\n", semkey);
+
+    //creation of semaphore/creation of the semaphore
+    semid = semget(semkey, 1, IPC_CREAT | IPC_EXCL | 0664);
+    if (DEBUG) printf("Semaphore created. Semid: %d\n", semid);
+
 //Creation
-    if (strcmp(args[1], "-c")) {
+    if (strncmp(args[1], "-c", strlen(args[1])) == 0) {
         //creation of file
         umask(0000);
-        f = open("./story.txt", O_CREAT, 0644);
+        f = open("story.txt", O_CREAT, 0644);
         umask(0022);
-        //creation of key
-        key = ftok("./story.txt", 12);
-        if (DEBUG) printf("Key created. Key: %d", key);
+
         //creation of shared memory for character num
-        shmid = shmget(key, 8, IPC_CREAT | 0664);
-        if (DEBUG) printf("Shared memory created. Shmid: %d", shmid);
-        //creation of the semaphore
-        semid = semget(key, 1, IPC_CREAT | 0664);
-        if (DEBUG) printf("Semaphore created. Semid: %d", semid);
+        shmid = shmget(memkey, 8, IPC_CREAT | 0664);
+        if (DEBUG) printf("Shared memory created. Shmid: %d\n", shmid);
+
+        //setting semaphore value
+        union semun su;
+        su.val = 1;
+        semscval = semctl(semid, 0, SETVAL, su);
+        if (DEBUG) printf("Smctl value set: %d\n", semscval);
+
+        //closing file
+        close(f);
     }
     //creation of shared memory
 
-//Removing
-    if (strcmp(args[1], "-r")) {
-        
+//Viewing
+    if (strncmp(args[1], "-v", strlen(args[1])) == 0) {
+        showstory();
     }
+
+//Removing
+    if (strncmp(args[1], "-r", strlen(args[1])) == 0) {
+        //removing semaphore
+        semscval = semctl(semid, 0, IPC_RMID);
+        if (DEBUG) printf("Sem is removed, val should be 0. Value: %d\n", semscval);
+
+        //removing shared memory
+        shmcval = shmctl(shmid, IPC_RMID, &shmemctl);
+        if (DEBUG) printf("Shared mem is removed, val should be 0. Value: %d\n", shmcval);
+
+        //printinf story*/
+        showstory();
+    //}
     return 0;
 }
